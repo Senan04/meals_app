76ce2c6162940c0a4d3ce052bf7d74e4b24fc55e
Senan <senanaslan29@t-online.de>
2024-10-18 21:34:47 +0200

    removed infoMessage and used its logic in onPressed, added PopScope to avoid exception when tried to used undo button even though MealDetailsScreen was closed, edited Star Icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -8,110 +8,110 @@ class MealDetailsScreen extends ConsumerWidget {
 
   final Meal meal;
 
-  //TODO add undo button
-  void _infoMessage(Meal? meal, BuildContext ctx) {
-    final mealAdded = meal == null;
-    ScaffoldMessenger.of(ctx).clearSnackBars();
-    ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
-      content: Text(
-        mealAdded
-            ? 'Meal was added to favorites'
-            : 'Meal was removed from favorites',
-      ),
-      // action: mealAdded
-      //     ? null
-      //     : SnackBarAction(
-      //         label: 'Undo',
-      //         onPressed: () => ref
-      //             .read(favoriteMealsProvider.notifier)
-      //             .toggleFavoriteStatus(meal),
-      //       ),
-    ));
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              final mealAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleFavoriteStatus(meal);
-              if (mealAdded) {
-                _infoMessage(null, context);
-              } else {
-                _infoMessage(meal, context);
-              }
-            },
-            icon: const Icon(Icons.star),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            for (final ingredient in meal.ingredients)
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: Text(
-                  ingredient,
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.onSurface),
-                ),
-              ),
-            const SizedBox(
-              height: 24,
-            ),
-            Text(
-              'Instructions',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                for (int i = 0; i < meal.steps.length; i++)
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
-                    child: Text(
-                      '${i + 1}. ${meal.steps[i]}',
-                      style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSurface),
-                      textAlign: TextAlign.start,
+    return PopScope(
+      //verhindert exception, wenn versucht wird auf undo button
+      //zu klicken obwohl MealDetailsScreen geschlossen
+      onPopInvokedWithResult: (didpop, result) =>
+          ScaffoldMessenger.of(context).clearSnackBars(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(meal.title),
+          actions: [
+            IconButton(
+              onPressed: () {
+                final mealAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      mealAdded
+                          ? 'Meal was added to favorites'
+                          : 'Meal was removed from favorites',
                     ),
+                    action: !mealAdded
+                        ? SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () => ref
+                                .read(favoriteMealsProvider.notifier)
+                                .toggleFavoriteStatus(meal),
+                          )
+                        : null,
                   ),
-              ],
+                );
+              },
+              icon: ref.watch(favoriteMealsProvider).contains(meal)
+                  ? const Icon(Icons.star)
+                  : const Icon(Icons.star_border_outlined),
             ),
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              for (final ingredient in meal.ingredients)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Text(
+                    ingredient,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface),
+                  ),
+                ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                'Instructions',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (int i = 0; i < meal.steps.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 5),
+                      child: Text(
+                        '${i + 1}. ${meal.steps[i]}',
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
